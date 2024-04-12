ce7828352ee2130805d25f0c77e74dfbb46cf3da
Voinopol Cosmin <cosmin072@gmail.com>
2024-04-12 18:41:45 +0300

    changed tests

--- src/test/java/inventory/model/InventoryTest.java
+++ src/test/java/inventory/model/InventoryTest.java
@@ -17,15 +17,31 @@ class InventoryTest {
     @BeforeEach
     void setUp() {
         inventory = new Inventory();
-        inventory.addProduct(new Product(1, "Product 1", 10.0, 10, 1, 100, null));
+        inventory.addProduct(new Product(1, "Product", 10.0, 10, 1, 100, null));
+        inventory.addProduct(new Product(2, "Product 2", 10.0, 10, 1, 100, null));
     }
 
     @ParameterizedTest
-    @DisplayName("should return product when found")
-    @ValueSource(strings = {"Product 1", "1"})
-    void shouldGetProductByName(String searchItem) {
-        Product expectedProduct = new Product(1, "Product 1", 10.0, 10, 1, 100, null);
+    @DisplayName("should return product when found 2")
+    @ValueSource(strings = {"Product 2", "2"})
+    void shouldGetProductByName2(String searchItem) {
+        Product expectedProduct = new Product(2, "Product 2", 10.0, 10, 1, 100, null);
+//
+        Product product = inventory.lookupProduct(searchItem);
 
+        assertEquals(expectedProduct.getProductId(), product.getProductId());
+        assertEquals(expectedProduct.getName(), product.getName());
+        assertEquals(expectedProduct.getPrice(), product.getPrice());
+        assertEquals(expectedProduct.getInStock(), product.getInStock());
+        assertEquals(expectedProduct.getMin(), product.getMin());
+        assertEquals(expectedProduct.getMax(), product.getMax());
+    }
+    @ParameterizedTest
+    @DisplayName("should return product when found")
+    @ValueSource(strings = {"Product", "1"})
+    void shouldGetProductByName(String searchItem) {
+        Product expectedProduct = new Product(1, "Product", 10.0, 10, 1, 100, null);
+//
         Product product = inventory.lookupProduct(searchItem);
 
         assertEquals(expectedProduct.getProductId(), product.getProductId());
@@ -38,7 +54,7 @@ class InventoryTest {
 
     @ParameterizedTest
     @DisplayName("should return empty product when not found")
-    @ValueSource(strings = {"Product 2", "2"})
+    @ValueSource(strings = {"abcdef"})
     void shouldReturnEmptyProductWhenNotFound(String searchItem) {
         Product expectedProduct = new Product(0, null, 0.0, 0, 0, 0, null);
 
@@ -53,7 +69,24 @@ class InventoryTest {
     }
 
     @Test
-    @DisplayName("should add product to list")
+    @DisplayName("should return product with id 0")
+    void shouldNotFoundEmptyList(){
+        Product expectedProduct = new Product(0, null, 0.0, 0, 0, 0, null);
+        inventory = new Inventory();
+
+        Product product = inventory.lookupProduct("abcdef");
+
+
+        assertEquals(expectedProduct.getProductId(), product.getProductId());
+        assertEquals(expectedProduct.getName(), product.getName());
+        assertEquals(expectedProduct.getPrice(), product.getPrice());
+        assertEquals(expectedProduct.getInStock(), product.getInStock());
+        assertEquals(expectedProduct.getMin(), product.getMin());
+        assertEquals(expectedProduct.getMax(), product.getMax());
+
+    }
+    @Test
+    @DisplayName("should throw error NullPointerException")
     void shouldThrowNullPointerException(){
 
         assertThrows(NullPointerException.class, () -> {
